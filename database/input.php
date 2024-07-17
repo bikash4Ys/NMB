f24a8b62ae1dfedc0c965bd02eb2a9ea269b7f09
Bikash4ys <230039ga@yse-c.net>
2024-07-17 12:17:55 +0900

    add & env for databaase has been creates

--- database/input.php
+++ database/input.php
@@ -5,21 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Shop</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container">
         <h2 class="h2">Items Registration</h2>
-        <form action="add.php" method="post">
+        <form action="add.php" method="post" enctype="multipart/form-data">
             <div class="mb-3 form-floating">
                 <input id="code" type="text" class="form-control" name="code" value="<?= @$item['code'] ?>">
-                <label for="code">Product Code</label>
+                <label for="code">Code</label>
                 <p class="text-danger pt-2"><?= @$errors['code'] ?></p>
             </div>
             <div class="mb-3 form-floating">
                 <input id="name" type="text" class="form-control" name="name" value="<?= @$item['name'] ?>">
-                <label for="name">Product Name</label>
+                <label for="name">Name</label>
                 <p class="text-danger pt-2"><?= @$errors['name'] ?></p>
             </div>
             <div class="mb-3 form-floating">
@@ -29,8 +30,13 @@
             </div>
             <div class="mb-3 form-floating">
                 <input id="stock" type="text" class="form-control" name="stock" value="<?= @$item['stock'] ?>">
-                <label for="stock">Quatity</label>
+                <label for="stock">Quantity</label>
                 <p class="text-danger pt-2"><?= @$errors['stock'] ?></p>
+            </div>
+            <div class="mb-3 form-floating">
+                <input id="image" type="file" class="form-control" name="image">
+                <label for="image">Image</label>
+                <p class="text-danger pt-2"><?= @$errors['image'] ?></p>
             </div>
             <button class="btn btn-primary">Registration</button>
             <a class="btn btn-outline-primary" href="./">Return</a>
